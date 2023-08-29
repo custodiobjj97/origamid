46dcc8468d72e7783c85a102527ba2d09838ce22
custodiobjj97 <custodiobjj97@outlook.com>
2023-08-29 17:46:20 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+import initScrollSuave from "./module/scroll-suave.js";
+import initAnimacaoScroll from "./module/scroll-animacao.js";
+import initAccordion from "./module/accordion.js";
+import initTabNav from "./module/tab-nav.js";
+import initModal from "./module/modal.js";
+import initDropdownMenu from "./module/dropdown-menu.js";
+import initMenuMobile from "./module/menu-mobile.js";
+import initFetchAnimais from "./module/fetch-animais.js";
+import initFetchBitcoin from "./module/fecth-bitcoin.js";
+
+initScrollSuave()
+initAnimacaoScroll()
+initAccordion()
+initTabNav()
+initModal()
+initDropdownMenu()
+initMenuMobile()
+initFetchAnimais()
+initFetchBitcoin()
