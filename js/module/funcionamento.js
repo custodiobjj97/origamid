ac44d4c96cd527d6e6d08fd1ac62e14a395a3306
custodiobjj97 <custodiobjj97@outlook.com>
2023-09-12 14:58:43 -0300

    update

--- js/module/funcionamento.js
+++ js/module/funcionamento.js
@@ -1,16 +1,39 @@
-export default function initFuncionamento() {
-    const functionamento = document.querySelector('[data-semana]')
-    const diasSemana = functionamento.dataset.semana.split(',').map(Number)
-    const horarioSemana = functionamento.dataset.horario.split(',').map(Number)
+export default class Funcionamento {
+    constructor(functionamento, activeClass) {
+        this.funcionamento = document.querySelector(functionamento);
+        this.activeClass = activeClass;
+    }
 
-    const dataAgora = new Date()
-    const diaAgora = dataAgora.getDay()
-    const horarioAgora = dataAgora.getHours()
-    
-    const semanaAberto = diasSemana.indexOf(diaAgora) !== -1
-    const horarioAberto = (horarioAgora >= horarioSemana[0] && horarioAgora < horarioSemana[1])
-    
-    if (semanaAberto && horarioAberto) {
-        functionamento.classList.add('aberto')
+    dadosFuncionamento() {
+        this.diasSemana = this.funcionamento.dataset.semana.split(',').map(Number);
+        this.horarioSemana = this.funcionamento.dataset.horario.split(',').map(Number);
+    }
+
+    dadosAgora() {
+        this.dataAgora = new Date();
+        this.diaAgora = this.dataAgora.getDay();
+        this.horarioAgora = this.dataAgora.getUTCHours() - 3;
+    }
+
+    estaAberto() {
+        const semanaAberto = this.diasSemana.indexOf(this.diaAgora) !== -1;
+        const horarioAberto = (this.horarioAgora >= this.horarioSemana[0]
+            && this.horarioAgora < this.horarioSemana[1]);
+        return semanaAberto && horarioAberto;
+    }
+
+    ativaAberto() {
+        if (this.estaAberto()) {
+            this.funcionamento.classList.add(this.activeClass);
+        }
+    }
+
+    init() {
+        if (this.funcionamento) {
+            this.dadosFuncionamento();
+            this.dadosAgora();
+            this.ativaAberto();
+        }
+        return this;
     }
 }
--- js/script.js
+++ js/script.js
@@ -1,20 +1,42 @@
 import ScrollSuave from "./module/scroll-suave.js";
-import initAnimacaoScroll from "./module/scroll-animacao.js";
-import initAccordion from "./module/accordion.js";
-import initTabNav from "./module/tab-nav.js";
-import initModal from "./module/modal.js";
-import initDropdownMenu from "./module/dropdown-menu.js";
-import initMenuMobile from "./module/menu-mobile.js";
-import initFetchAnimais from "./module/fetch-animais.js";
-import initFetchBitcoin from "./module/fecth-bitcoin.js";
+import Accordion from "./module/accordion.js";
+import TabNav from "./module/tab-nav.js";
+import Modal from "./module/modal.js";
+import Tooltip from "./module/tooltip.js";
+import DropdownMenu from "./module/dropdown-menu.js";
+import Funcionamento from "./module/funcionamento.js";""
+import MenuMobile from "./module/menu-mobile.js";
+import fetchAnimais from "./module/fetch-animais.js";
+import fetchBitcoin from "./module/fecth-bitcoin.js";
+import ScrollAnima  from './module/scroll-animacao.js'
+
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]')
 scrollSuave.init()
-initAnimacaoScroll()
-initAccordion()
-initTabNav()
-initModal()
-initDropdownMenu()
-initMenuMobile()
-initFetchAnimais()
-initFetchBitcoin()
+const accordion = new Accordion('[data-anime="accordion"] dt')
+accordion.init()
+
+const tabNav = new TabNav('[data-tab="menu"] li', '[data-tab="content"] section')
+
+tabNav.init()
+
+const modal = new Modal('[data-modal="abrir"]', '[data-modal="fechar"]', '[data-modal="container"]')
+
+modal.init()
+
+const tooltip = new Tooltip('[data-tooltip]')
+tooltip.init()
+
+const scrollAnima = new ScrollAnima('[data-anime="scroll"]');
+scrollAnima.init();
+
+const dropdownMenu = new DropdownMenu('[data-dropdown]');
+dropdownMenu.init();
+
+const menuMobile = new MenuMobile('[data-menu="button"]', '[data-menu="list"]');
+menuMobile.init();
+
+const funcionamento = new Funcionamento('[data - semana]', 'aberto')
+funcionamento.init()
+fetchAnimais('../../animaisapi.json', '.numeros-grid');
+fetchBitcoin('https://blockchain.info/ticker', '.btc-preco')
